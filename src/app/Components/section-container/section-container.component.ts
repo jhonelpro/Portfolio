b1f86676b9593cc6291a787b9f0987beb6b18cbe
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-03 00:09:53 -0300

    feat: sections and components were added to the main

---
+++ src/app/Components/section-container/section-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-container.component.html',
+  styleUrl: './section-container.component.css'
+})
+export class SectionContainerComponent {
+
+}
--- src/app/Pages/portfolio-home/portfolio-home.component.ts
+++ src/app/Pages/portfolio-home/portfolio-home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../Components/header/header.component";
+import { SectionContainerComponent } from "../../Components/section-container/section-container.component";
+import { WorkExperienceComponent } from "../../Components/work-experience/work-experience.component";
+import { HomeComponent } from "../../Components/home/home.component";
+import { ProyectsComponent } from "../../Components/proyects/proyects.component";
+import { SkillsComponent } from "../../Components/skills/skills.component";
 
 @Component({
   selector: 'app-portfolio-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SectionContainerComponent, WorkExperienceComponent, HomeComponent, ProyectsComponent, SkillsComponent],
   templateUrl: './portfolio-home.component.html',
   styleUrl: './portfolio-home.component.css'
 })
