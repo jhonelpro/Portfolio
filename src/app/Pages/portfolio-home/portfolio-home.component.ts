60284ef72437a803b7e24d3282643595d9708299
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-06 23:08:58 -0300

    refactor: the projects component is renamed and the paragraph associated with the projects is fixed

--- src/app/Pages/portfolio-home/portfolio-home.component.ts
+++ src/app/Pages/portfolio-home/portfolio-home.component.ts
@@ -3,14 +3,14 @@ import { HeaderComponent } from "../../Components/header/header.component";
 import { SectionContainerComponent } from "../../Components/section-container/section-container.component";
 import { WorkExperienceComponent } from "../../Components/work-experience/work-experience.component";
 import { HomeComponent } from "../../Components/home/home.component";
-import { ProyectsComponent } from "../../Components/proyects/proyects.component";
 import { SkillsComponent } from "../../Components/skills/skills.component";
 import { ContactformComponent } from "../../Components/contactform/contactform.component";
+import { ProjectsComponent } from "../../Components/projects/projects.component";
 
 @Component({
   selector: 'app-portfolio-home',
   standalone: true,
-  imports: [HeaderComponent, SectionContainerComponent, WorkExperienceComponent, HomeComponent, ProyectsComponent, SkillsComponent, ContactformComponent],
+  imports: [HeaderComponent, SectionContainerComponent, WorkExperienceComponent, HomeComponent, ProjectsComponent, SkillsComponent, ContactformComponent, ProjectsComponent],
   templateUrl: './portfolio-home.component.html',
   styleUrl: './portfolio-home.component.css'
 })
